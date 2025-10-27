134b89f560da7f74b4e3cfbb1701482aeb009927
ienming <hsieh.alan.0726@gmail.com>
2025-10-27 21:08:07 +0800

    chore: add cloudinary config for strapi cloud

---
+++ config/env/production/middlewares.ts
@@ -0,0 +1,36 @@
+module.exports = ({ env }) => [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // Cloudinary images
+            // Add other necessary image sources if needed
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            // Add other necessary media sources if needed
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
